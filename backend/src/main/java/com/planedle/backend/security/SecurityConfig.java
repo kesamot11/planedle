fa0356a579b361a8a9d46673e358693a28dd80e4
Tomas Holinka <T.Holinka@student.tudelft.nl>
2025-06-08 14:49:53 +0200

    Fixed blocked random-aircraft endpoint

--- backend/src/main/java/com/planedle/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/planedle/backend/security/SecurityConfig.java
@@ -26,9 +26,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
+                .cors()
+                .and()
+                .csrf().disable()
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/**", "/api/random-aircraft").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
